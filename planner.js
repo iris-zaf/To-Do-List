d133bac534562117352e8e6ccb93b4d2a772b03b
iris-zaf <zafi2313@hotmail.com>
2022-09-05 22:46:19 +0300

    added js

--- planner.js
+++ planner.js
@@ -1,8 +1,51 @@
-window.addEventListener('load',() =>{
-    const form = document.querySelector('#new-task-form');
-    const input = document.querySelector('#new-task-input');
-const list_el = document.querySelector('#tasks');
+window.addEventListener('load', (event) => {
+    const form = document.querySelector("#new-task-form");
+    const input = document.querySelector ("#new-task-input");
+    const list_el = document.querySelector("#tasks");
+    const image = document.createElement("img");
+    image.src = "planner-img.webp";
+    document.body.appendChild(image);
+    image.classList.add("image");
+    form.addEventListener ('submit', (event) => {
+        event.preventDefault();
+      
+
+        const task = input.value;
+
+        if (!task) {
+            alert ("Please fill out the task");
+            return;
+        } 
+        const task_el = document.createElement("div");
+        task_el.classList.add("task");
+
+        const task_content_el = document.createElement("div");
+        task_content_el.classList.add("content");
+        task_content_el.innerText = task;
+
+        task_el.appendChild(task_content_el);
+
+        const task_actions_el = document.createElement("div");
+        task_actions_el.classList.add("actions");
+
+        const task_edit_el = document.createElement("button");
+        task_actions_el.classList.add("edit");
+        task_edit_el.innerHTML = "Edit";
+
+        const task_delete_el = document.createElement("button");
+        task_actions_el.classList.add("delete");
+       task_delete_el.innerHTML = "Delete";
+
+        
+        task_actions_el.appendChild(task_edit_el);
+        task_actions_el.appendChild(task_delete_el);
+
+        task_el.appendChild(task_actions_el);
 
 
-}
-)
+        list_el.appendChild(task_el);
+
+        input.value = "";
+
+    })
+})
